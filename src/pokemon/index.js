214f6f58b080a89ed091f3ab2ae372435ab9fa33
phat@minim1 <phat.tranducvn@gmail.com>
2022-11-26 12:55:05 -0800

    Add modal for consumable items

--- src/pokemon/index.js
+++ src/pokemon/index.js
@@ -1,13 +1,29 @@
-const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementById("close-modal-btn");
+const modals = document.querySelectorAll(".modal");
+const closeModalButtons = document.querySelectorAll(".modal__header__close-btn");
 const pokemonCards = document.querySelectorAll(".card.hoverable");
 
-pokemonCards.forEach(element => {
-    element.addEventListener("click", (e) => {
-        modal.classList.add("show");
-    });
+// Close all modals
+function closeAllModals() {
+    modals.forEach(modal => {
+        modal.classList.remove("show");
+    })
+}
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", closeAllModals);
 })
 
-closeModalBtn.addEventListener("click", (e) => {
-	modal.classList.remove("show");
+// Open Pokemon Modal
+pokemonCards.forEach((element) => {
+	element.addEventListener("click", (e) => {
+        const pokemonModal = document.getElementById("pokemon-modal");
+        pokemonModal.classList.add("show");
+	});
 });
+
+// Open Feed Modal
+const openUseItemsModalButton = document.getElementById("open-use-items-modal-btn")
+const useItemsModal = document.getElementById("use-items-modal");
+openUseItemsModalButton.addEventListener('click', e=> {
+    closeAllModals();
+    useItemsModal.classList.add("show");
+})
