2867e9dabd0225c7b9a4922b5f350684af665e92
phat@minim1 <phat.tranducvn@gmail.com>
2022-11-26 13:48:30 -0800

    Add functionality for switching item tabs

--- src/pokemon/index.js
+++ src/pokemon/index.js
@@ -20,10 +20,49 @@ pokemonCards.forEach((element) => {
 	});
 });
 
-// Open Feed Modal
+// Open Use items Modal
 const openUseItemsModalButton = document.getElementById("open-use-items-modal-btn")
 const useItemsModal = document.getElementById("use-items-modal");
 openUseItemsModalButton.addEventListener('click', e=> {
     closeAllModals();
     useItemsModal.classList.add("show");
+})
+
+// Show content for items
+const openConsumableBtn = document.getElementById("open-consumable-items");
+const openMysticBtn = document.getElementById("open-mystic-items");
+const consumableItemsContainer = document.getElementById("consumable-items");
+const mysticItemsContainer = document.getElementById("mystic-items");
+
+openConsumableBtn.addEventListener('click', e => {
+    consumableItemsContainer.classList.add("show");
+    openConsumableBtn.parentElement.classList.add("active");
+    
+    mysticItemsContainer.classList.remove("show");
+    openMysticBtn.parentElement.classList.remove("active");
+})
+
+openMysticBtn.addEventListener('click', e => {
+    mysticItemsContainer.classList.add("show");
+    openMysticBtn.parentElement.classList.add("active");
+
+    consumableItemsContainer.classList.remove("show");
+    openConsumableBtn.parentElement.classList.remove("active");
+})
+
+// Select items
+const itemInputs = document.querySelectorAll("input.item-amount");
+
+itemInputs.forEach(itemInput => {
+    itemInput.addEventListener('change', e => {
+        const {value, id} = e.target;
+        const containerId = `item-${id.split("-amount")[0]}`;
+        const containerElement = document.getElementById(containerId);
+
+        if (value > 0) {
+            containerElement.classList.add("selected");
+        } else {
+            containerElement.classList.remove("selected");
+        }
+    })
 })
