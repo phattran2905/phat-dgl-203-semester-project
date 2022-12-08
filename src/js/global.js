25d05aac0889252e728dbafd0b499349605b5a1c
phat@minim1 <phat.tranducvn@gmail.com>
2022-12-07 23:55:56 -0800

    Add responsive styles for signup page

--- src/js/global.js
+++ src/js/global.js
@@ -1,10 +1,10 @@
-const SERVER_API_URL = "http://0.0.0.0:8080/api/";
-const CLIENT_ORIGIN_URL = window.location.origin;
-const CLIENT_ROUTES = [{
-    loginPage: `${CLIENT_ORIGIN_URL}/auth/login.html`,
-    signupPage: `${CLIENT_ORIGIN_URL}/auth/signup.html`,
-    profilePage: `${CLIENT_ORIGIN_URL}/auth/profile.html`
-}]
+// Navigation
+const menuButton = document.getElementById("menu")
+menuButton.addEventListener("click", e => {
+    
+})
+
+
 
 // ===== Handle Modals =====
 const modals = document.querySelectorAll(".modal");
